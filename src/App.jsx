4e541f9d5a06236334de8369aebcc22221ae0c69
Analia Paiva <beatrizrace131@gmail.com>
2024-07-20 15:17:37 -0400

    Added css

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import ChatBot from './components/ChatBot.jsx';
 //import Main from './components/pages/Main.jsx';
 const App = () => {
   const [response, setResponse] = useState('');
-  
+  const [loading, setLoading] = useState(false);
   let [places, setPlaces] = useState( []);
   
 
@@ -16,10 +16,14 @@ const App = () => {
     setInput(event.target.value);
   };
 
+  const handleLoading = (loadingStatus) => {
+    setLoading(loadingStatus);
+  }
+
   const handleSubmit = (e) => {
     e.preventDefault();
     if (input.trim() !== '') {
-      console.log('Mensaje enviado:', inputValue);
+      console.log('Mensaje enviado:', input);
       // logica para ernviar el mensaje
       setInput(''); 
     }
@@ -44,17 +48,17 @@ const App = () => {
   };
   return (
     <>
-      <div className="w-screen m-0 p-3 bg-slate-500 flex items-center justify-center">
-        <div className="flex flex-col items-center">
-          <h1 className="text-3xl font-bold mb-2">MAPAS</h1>
-          <LoaderModal/>
-          </div>
+      <div className="w-screen max-h-12 m-0 p-3 bg-indigo-500 flex items-center justify-center py-16">
+        <h1 className="title-font text-3xl font-extrabold tracking-tight sm:text-4xl mr-4 text-white ">Descubriendo Encarnación</h1>
+        <div className="text-center w-20">
+          <LoaderModal isOpen={loading}/>
         </div>
-      <div>
-      <ChatBot getResponse={getResponse}/>
+        <ChatBot getResponse={getResponse} handleLoading={handleLoading}/>
       </div>
-      <MapComponent locations={places} />
-      <h1 className="text-4xl font-bold mb-4 text-center">RESULTADOS</h1>
+     <div className="w-50 h-50">
+           <MapComponent locations={places} />
+      </div>
+      <h1 className="title-font text-4xl text-indigo-500 font-bold mb-4 text-center m-10">Tendencias</h1>
       <div className='flex items-center'>
       {places && places.map(place => (
         <Card
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,11 @@
 const Card = ({ nombre, tipo, direccion, descripcion }) => {
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg m-4 w-full h-full">
+    <div className="max-w-sm rounded overflow-hidden shadow-lg m-4 w-80 h-80">
       <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2">{nombre}</div>
-        <div className="font-bold text-slate-500 text-sm mb-2">{tipo}</div>
-        <p className="text-gray-700 text-base">{descripcion}</p>
-        <p className="font-bold text-slate-700 text-sm">{direccion}</p>
+        <div className="font-bold font-sans text-xl mb-2">{nombre}</div>
+        <div className="font-bold font-sans text-slate-500 text-sm mb-2">{tipo}</div>
+        <p className="font-sans text-gray-700 text-base">{descripcion}</p>
+        <p className="font-sans font-bold text-slate-700 text-sm">{direccion}</p>
       </div>
     </div>
   );
