c15c1d4bcfc3fabb4aeaf2c450e9be640658c28d
Analia Paiva <beatrizrace131@gmail.com>
2024-07-20 13:29:36 -0400

    commit desde development

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,15 @@
 import { useState } from 'react'
 import MapComponent from './components/Map.jsx'
-import Chat from './components/ChatBot.jsx';
 
 const App = () => {
   let [places, setPlaces] = useState([]);
 
   return (
     <>
-    <div className="bg-gray-100 flex justify-center items-center min-h-screen">
-      <div className='flex w-full space-x-4 max-w-7xl mx-auto'> 
-        <div className="w-1/2 p-4 bg-blue-500 text-white"> 
-          <MapComponent locations={places}/>
-        </div>
-        <div className="w-1/2 p-4 bg-gray-400 text-black">
-        <Chat/> 
-        </div>
+      <div className="w-screen h-screen m-0 p-3 bg-slate-500">
+        <MapComponent locations={places}/>
       </div>
-    </div>
-  </>
-  
+    </>
   );
 };
 
